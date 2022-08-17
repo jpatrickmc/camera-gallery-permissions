a7f2cd3ab3f4768a3a52f34a7ccb01270134cd90
Patrick Mcdaniel <patrickmcdaniel@DALML7180.local>
2022-08-17 14:30:24 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      console.log("cameraPermission: ", cameraPermission);
+      const galleryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasGalleryPermission(galleryPermission.status == "granted");
+      console.log("galleryPermission: ", galleryPermission);
+    })();
+  }, []);
+
+  console.log("hasCameraPermission: ", hasCameraPermission);
+  console.log("hasGalleryPermission: ", hasGalleryPermission);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.labels}>
+        {hasCameraPermission ? "Camera : Granted" : "Camera : Denied"}
+      </Text>
+      <Text style={styles.labels}>
+        {hasGalleryPermission ? "Gallery : Granted" : "Gallery : Denied"}
+      </Text>
     </View>
   );
 }
@@ -13,8 +36,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "grey",
+  },
+  labels: {
+    color: "white",
+    fontSize: 24,
+    padding: 10,
   },
 });
